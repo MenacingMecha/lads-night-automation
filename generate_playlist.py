07535e5282deea24c2b4ea0e628b1c8083de637c
Callum Mackay <mackaycallum99@gmail.com>
2020-07-01 16:16:09 +0100

    Now populates clip groups and playlist from yaml config file

--- generate_playlist.py
+++ generate_playlist.py
@@ -3,13 +3,12 @@
 import os
 import glob
 import random
+import yaml
 from typing import List, TextIO
 
 class Playlist:
-    def __init__(self, playlist_file: TextIO, inbetweens: List[str], num_of_inbetweens: int):
+    def __init__(self, playlist_file: TextIO):
         self._playlist_file = playlist_file
-        self._inbetweens = inbetweens
-        self._num_of_inbetweens = num_of_inbetweens
         self._temp_blocklist_additions = []
     
     def add_clip(self, path_to_clip: str, add_to_temp_blocklist: bool):
@@ -17,16 +16,6 @@ class Playlist:
         self._playlist_file.write("\n")
         if add_to_temp_blocklist:
             self._temp_blocklist_additions.append(path_to_clip)
-    
-    def add_show(self, path_to_show: List[str], should_end_with_inbetweens: bool):
-        while len(path_to_show) > 0:
-            self.add_clip(path_to_show.pop(0), False)
-            should_add_inbetween = True
-            if len(path_to_show) == 0 and not should_end_with_inbetweens:
-                should_add_inbetween = False
-            if should_add_inbetween:
-                for x in range(self._num_of_inbetweens):
-                    self.add_clip(self._inbetweens.pop(), True)
     
     def get_temp_blocklist_additions(self) -> List[str]:
         return self._temp_blocklist_additions
@@ -42,7 +31,7 @@ class ClipGroups:
     def add_clips_from_dir(self, group_name: str, path_to_dir: str, randomise_order: bool):
         clips = []
         for ext in ('*.mp4', '*.mkv'):
-            clips.extend(glob.glob(os.path.join(path_to_dir, ext)))
+            clips.extend(glob.glob(os.path.join(os.path.expanduser(path_to_dir), ext)))
         for clip in clips:
             if clip in self._temp_blocklist:
                 clips.remove(clip)
@@ -53,33 +42,25 @@ class ClipGroups:
     def get_clip_from_group(self, group_name: str) -> str:
         return self._clip_groups[group_name].pop(0)
     
-    def get_clip_group(self, group_name: str) -> List[str]:
-        return self._clip_groups[group_name]
-
 if __name__ == "__main__":
     temp_blocklist_path = "temp_blocklist.txt"
-    try:
-        os.path.isfile(temp_blocklist_path)
-    except NameError:
-        clip_groups = ClipGroups(None)
-    else:
+    if (os.path.isfile(temp_blocklist_path)):
         with open(temp_blocklist_path, "r") as temp_blocklist_file:
             clip_groups = ClipGroups(temp_blocklist_file)
-    clip_groups.add_clips_from_dir("intro", "intro", True)
-    clip_groups.add_clips_from_dir("inbetween", "inbetween", True)
-    clip_groups.add_clips_from_dir("intermission", "intermission", True)
-    clip_groups.add_clips_from_dir("outro", "outro", True)
-    clip_groups.add_clips_from_dir("show_01", "show_01", False)
-    clip_groups.add_clips_from_dir("show_02", "show_02", False)
-    clip_groups.add_clips_from_dir("show_03", "show_03", False)
-    playlist_path = "playlist.txt"
-    with open(playlist_path, "w") as playlist_file:
-        playlist = Playlist(playlist_file, clip_groups.get_clip_group("inbetween"), 2)
-        playlist.add_clip(clip_groups.get_clip_from_group("intro"), False)
-        playlist.add_show(clip_groups.get_clip_group("show_01"), True)
-        playlist.add_show(clip_groups.get_clip_group("show_02"), False)
-        playlist.add_clip(clip_groups.get_clip_from_group("intermission"), False)
-        playlist.add_show(clip_groups.get_clip_group("show_03"), False)
-        playlist.add_clip(clip_groups.get_clip_from_group("outro"), True)
+    else:
+        clip_groups = ClipGroups(None)
+
+    config_path = "lads-night.yaml"
+    with open(config_path) as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+        for key, value in config["clip groups"].items():
+            clip_groups.add_clips_from_dir(key, value["path"], value["shuffle"])
+
+        playlist_path = "playlist.txt"
+        with open(playlist_path, "w") as playlist_file:
+            playlist = Playlist(playlist_file)
+            for group in config["schedule"]:
+                playlist.add_clip(clip_groups.get_clip_from_group(group), config["clip groups"][group]["blocklist type"] == "temporary")
+
     with open(temp_blocklist_path, "w") as temp_blocklist_file:
         temp_blocklist_file.writelines(x + "\n" for x in playlist.get_temp_blocklist_additions())
