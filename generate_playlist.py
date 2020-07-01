61db9ab6421e13bc1636dee909f8e29c2e31db44
Callum Mackay <mackaycallum99@gmail.com>
2020-07-01 18:15:14 +0100

    Now supports a permanent block type

--- generate_playlist.py
+++ generate_playlist.py
@@ -4,53 +4,74 @@ import os
 import glob
 import random
 import yaml
+import logging
 from typing import List, TextIO
 
 class Playlist:
     def __init__(self, playlist_file: TextIO):
         self._playlist_file = playlist_file
         self._temp_blocklist_additions = []
+        self._perm_blocklist_additions = []
     
-    def add_clip(self, path_to_clip: str, add_to_temp_blocklist: bool):
-        self._playlist_file.write(path_to_clip)
-        self._playlist_file.write("\n")
-        if add_to_temp_blocklist:
-            self._temp_blocklist_additions.append(path_to_clip)
+    def add_clip(self, path_to_clip: str, blocklist_type: str):
+        if path_to_clip is not None:
+            self._playlist_file.write(path_to_clip)
+            self._playlist_file.write("\n")
+            if blocklist_type == "temporary":
+                self._temp_blocklist_additions.append(path_to_clip)
+            elif blocklist_type == "permanent":
+                self._perm_blocklist_additions.append(path_to_clip)
     
     def get_temp_blocklist_additions(self) -> List[str]:
         return self._temp_blocklist_additions
 
+    def get_perm_blocklist_additions(self) -> List[str]:
+        return self._perm_blocklist_additions
+
 class ClipGroups:
-    def __init__(self, temp_blocklist_file: TextIO):
-        if temp_blocklist_file is not None:
-            self._temp_blocklist = temp_blocklist_file.read().splitlines()
-        else:
-            self._temp_blocklist = []
+    def __init__(self, temp_blocklist: List[str], perm_blocklist: List[str]):
+        self._temp_blocklist = temp_blocklist
+        self._perm_blocklist = perm_blocklist
         self._clip_groups = {}
     
     def add_clips_from_dir(self, group_name: str, path_to_dir: str, randomise_order: bool):
         clips = []
         for ext in ('*.mp4', '*.mkv'):
             clips.extend(glob.glob(os.path.join(os.path.expanduser(path_to_dir), ext)))
+        valid_clips = []
         for clip in clips:
-            if clip in self._temp_blocklist:
-                clips.remove(clip)
+            if clip not in self.__get_blocklist():
+                valid_clips.append(clip)
+        if len(valid_clips) == 0:
+            logging.warning("Group '" + group_name + "' has no valid clips.") 
         if randomise_order:
-            random.shuffle(clips)
+            random.shuffle(valid_clips)
         else:
-            clips.sort()
-        self._clip_groups[group_name] = clips
+            valid_clips.sort()
+        self._clip_groups[group_name] = valid_clips
 
     def get_clip_from_group(self, group_name: str) -> str:
-        return self._clip_groups[group_name].pop(0)
+        if (len(self._clip_groups[group_name]) > 0):
+            return self._clip_groups[group_name].pop(0)
+        else:
+            return None
     
+    def __get_blocklist(self) -> List[str]:
+        return self._temp_blocklist + self._perm_blocklist
+
+def get_blocklist_from_file(path: str) -> List[str]:
+    if (os.path.isfile(path)):
+        with open(path, "r") as file:
+            blocklist = file.read().splitlines()
+    else:
+        blocklist = []
+    return blocklist
+
 if __name__ == "__main__":
     temp_blocklist_path = "temp_blocklist.txt"
-    if (os.path.isfile(temp_blocklist_path)):
-        with open(temp_blocklist_path, "r") as temp_blocklist_file:
-            clip_groups = ClipGroups(temp_blocklist_file)
-    else:
-        clip_groups = ClipGroups(None)
+    perm_blocklist_path = "perm_blocklist.txt"
+    clip_groups = ClipGroups(get_blocklist_from_file(temp_blocklist_path), get_blocklist_from_file(perm_blocklist_path))
+    # print(clip_groups._temp_blocklist + clip_groups._perm_blocklist)
 
     config_path = "lads-night.yaml"
     with open(config_path) as file:
@@ -62,7 +83,10 @@ if __name__ == "__main__":
         with open(playlist_path, "w") as playlist_file:
             playlist = Playlist(playlist_file)
             for group in config["schedule"]:
-                playlist.add_clip(clip_groups.get_clip_from_group(group), config["clip groups"][group]["blocklist type"] == "temporary")
+                playlist.add_clip(clip_groups.get_clip_from_group(group), config["clip groups"][group]["blocklist type"])
 
     with open(temp_blocklist_path, "w") as temp_blocklist_file:
         temp_blocklist_file.writelines(x + "\n" for x in playlist.get_temp_blocklist_additions())
+
+    with open(perm_blocklist_path, "a") as perm_blocklist_file:
+        perm_blocklist_file.writelines(x + "\n" for x in playlist.get_perm_blocklist_additions())
